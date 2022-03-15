f8344694e8949d2201e477c4236d46973e4db239
Giulio Cocconi <coccogiulio8@gmail.com>
2022-03-15 19:35:08 +0100

    Changed script directory

---
+++ scripts/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+config_dir=$1
+scripts_dir=$2
+check_file=$3
+
+ready=1
+
+function check_command() {
+	if ! command -v $1 &> /dev/null; then
+		echo "[error] $1 not found, please install it yourself"
+		ready=0
+	fi
+}
+
+if [[ -f $check_file ]]; then
+	echo "[error] deps alredy installed!"
+	exit 1
+fi
+
+echo "Welcome to the install/upgrade script..."
+echo
+echo "Installing deps..."
+
+check_command "python3"
+check_command "perl"
+check_command "vifm"
+
+[[ $ready != 1 ]] && exit 1
+
+pip install pynvim
+
+echo "All done!!!"
+touch $check_file
